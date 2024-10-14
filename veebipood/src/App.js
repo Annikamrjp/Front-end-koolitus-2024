1d02d2b661b8db031c43116b7656c6d28cfd2302
Annikamrjp <annika.marjapuu204@gmail.com>
2024-10-14 16:58:01 +0300

    Reactiga alustamine

---
+++ veebipood/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <button className="nupp">Nupp</button>
+
+     <img className="pilt" src="https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg" alt="kassi pilt" />
+    </div>
+  );
+}
+
+export default App;
